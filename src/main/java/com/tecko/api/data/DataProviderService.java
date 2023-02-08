ce847d4dfbc27c4725836054f64f3b2ba70021d2
Zdenko <z.skalicky@gmail.com>
2023-02-08 21:08:23 +0100

    Added integration.

--- src/main/java/com/tecko/api/data/DataProviderService.java
+++ src/main/java/com/tecko/api/data/DataProviderService.java
@@ -1,8 +1,37 @@
 package com.tecko.api.data;
 
+import com.mifmif.common.regex.Generex;
+import lombok.Getter;
+import lombok.Setter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DataProviderService {
 
+    private static final Logger LOG = LoggerFactory.getLogger(DataProviderService.class);
+
+    @Getter
+    @Setter
+    private String value;
+
+    private final String pattern = "([a-z]{20})";
+
+    public DataProviderService() {
+        this.setValue(generateString());
+    }
+
+    @Scheduled(initialDelay=5000,fixedRate = 5000)
+    private void scheduleItem(){
+        String oldVal = this.getValue();
+        this.setValue(generateString());
+
+        LOG.info(String.format("Scheduled event run: %s/%s", oldVal, this.getValue()));
+    }
+
+    private String generateString() {
+        return new Generex(pattern).random();
+    }
 }
